923fa53d83dafc7f0ca328fd3511997b192301da
RKatana <roduor41@gmail.com>
2018-12-31 12:40:08 +0300

    updated the models and added the functions

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 # from distutils.command.upload import upload
 import datetime as dt
 from builtins import classmethod
+from django.contrib.auth.models import User
 
 from django.db import models
 from django.forms.fields import CharField, ImageField
@@ -8,35 +9,36 @@ from django.forms.fields import CharField, ImageField
 
 # Create your models here.
 class Profile(models.Model):
-    profile_photo=models.ImageField(upload_to='profile/')
-    user_name = models.CharField(max_length=30)
+    user = models.OneToOneField(User)
+    profile_photo=models.ImageField(upload_to='images/')
     email=models.EmailField()
     bio= models.CharField(max_length=100)
 
-    def __str__:
-        return self.user_name
+    def __str__(self):
+        return self.user.username
 
     @classmethod
     def search_user(cls,username):
-        user=cls.objects.filter(user_name__iexact=username)
+        user=cls.objects.filter(user__username__icontains=username)
         return user
+    
+    def save_profile(self):
+        self.save()
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='articles/')
+    image = models.ImageField(upload_to='images/')
     image_name= models.CharField(max_length=60)
     image_caption =models.CharField(max_length=60)
     uploaded_on = models.DateTimeField(auto_now_add=True)
     comments=models.TextField()
-    user_profile =models.ForeignKey(Profile)
+    user_profile =models.ForeignKey(User)
 
-    def __str__:
+    def __str__(self):
         return self.image_name
-    @classmethod
+
     def save_image(self):
-        cls.save()
-    @classmethod
-    def del_image(self):
-        cls.delete()
+        self.save()
+    
     def update_caption(self,image_caption):
         self.update()
 
